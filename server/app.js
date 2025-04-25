fb716b5f4c6a618ed4bad1e890a49017acf3ea26
Akash-ojha-AJ2 <jhak2594@gmail.com>
2025-04-25 19:00:19 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
